ab5677df36f90f63d2bfb4491af6a5a4654e95ec
Ali Ghorashi <aligho@mit.edu>
2021-04-12 13:05:19 -0400

    plot_mpb

--- src/MPB_ANALYSIS.jl
+++ src/MPB_ANALYSIS.jl
@@ -3,6 +3,10 @@ module MPB_ANALYSIS
 #Dependencies
 using Plots
 
+include("plot_mpb.jl")
+export plot_mpb
+
 greet() = print("Hello World!")
+
 
 end # module
---
+++ src/plot_mpb.jl
@@ -0,0 +1,19 @@
+function plot_mpb(filename::String, mode::String, nbands::Int)
+    Bands = Vector{Vector{Float64}}()
+    for line in readlines(filename)
+        if contains(line, mode*"freqs")
+            try
+                push!(Bands, parse.(Float64, string.(chop.(string.(split(line))))[end-nbands+1:end]))
+            catch 
+            end
+        end
+    end
+    numkpoints = length(Bands)
+    numbands = length(Bands[1])
+    reshapedBands = Array{Float64, 2}(undef, (numkpoints, numbands))
+    for k in 1:numkpoints
+       reshapedBands[k, :] = Bands[k]
+    end
+    #return Bands 
+    return reshapedBands
+end
