5971d7343367cab1a6391f62e4928e3f7897766e
Ali Ghorashi <aligho@mit.edu>
2021-04-21 03:11:09 -0400

    plottopologybands

--- src/plot_topologybands.jl
+++ src/plot_topologybands.jl
@@ -1,17 +1,25 @@
 function plot_topologybands(filename::String)
     Bands = Vector{Vector{Float64}}()
-    reshapedBands = Array{Float64, 2}(undef, (length(Bands), length(Bands[1])))
     try
         for line in readlines(filename)
-            push!(Bands, parse.(Ref(Float64), string.(split(replace(line, "," => " "))))[6:end], "\n")
+            push!(Bands, parse.(Ref(Float64), string.(split(replace(line, "," => " "))))[6:end])
         end
     catch e
         for line in readlines(filename*".out")
-            push!(Bands, parse.(Ref(Float64), string.(split(replace(line, "," => " "))))[6:end], "\n")
+            push!(Bands, parse.(Ref(Float64), string.(split(replace(line, "," => " "))))[6:end])
         end
     end
+    reshapedBands = Array{Float64, 2}(undef, (length(Bands), length(Bands[1])))
     for i in 1:length(Bands)
         reshapedBands[i, :] = Bands[i] ##Note that each row now corresponds to a point in reciprocal space, as desired
     end
-    plot(reshapedBands)
+    plot(reshapedBands, legend=false, xticks=false, size=(1000, 500), linestyle=:dash, color="pink", linewidth=5)
+end
+
+function plot_manytopologybands(directory::String)
+    plot_array = Vector{Tuple{String, Plots.Plot}}()
+    for filename in readdir(directory)
+        push!(plot_array, (filename, plot_topologybands(filename) ))
+    end
+    return plot_array
 end
