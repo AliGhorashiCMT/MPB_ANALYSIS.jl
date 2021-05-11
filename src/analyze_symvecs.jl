edfb1a4e94c4882ba349d1f10e19664659e89261
Ali Ghorashi <aligho@mit.edu>
2021-05-10 21:37:10 -0400

    analyze symvecs

--- src/analyze_symvecs.jl
+++ src/analyze_symvecs.jl
@@ -5,5 +5,10 @@ function analyze_symvecs(sgnum::Integer, D::Integer=2, id::Integer=1, res::Integ
     symeigsd, lgirsd = read_symdata(symeig_filename, sgnum=sgnum, D=D, dir=symeigdir)
     #println(symeigsd)
     #println(symeigsd["Γ"])
-    println(MPBUtils.symeigs2irreps(symeigsd["Γ"], get_lgirreps(sgnum, D)["Γ"], 1:2))
+    symvec1 = convert.(Integer, round.(MPBUtils.symeigs2irreps(symeigsd["Γ"], get_lgirreps(sgnum, D)["Γ"], 1:4), digits=3))
+    symvec2 = convert.(Integer, round.(MPBUtils.symeigs2irreps(symeigsd["K"], get_lgirreps(sgnum, D)["K"], 1:4), digits=3))
+    symvec3 = convert.(Integer, round.(MPBUtils.symeigs2irreps(symeigsd["M"], get_lgirreps(sgnum, D)["M"], 1:4), digits=3))
+    symvec = [symvec1..., symvec2..., symvec3...]
+    #calc_detailed_topology(symvec, sgnum, D)
+    calc_topology(symvec, BRS)
 end
