bc6a098d3cfe737c1aa762b9dc267a75df42396c
Ali Ghorashi <aligho@mit.edu>
2021-11-15 14:50:16 -0500

    Tri rod dispersion png

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MPB_ANALYSIS.jl
+
+![Tri_rod]
+
+[Tri_rod]: https://github.com/AliGhorashiCMT/MPB_ANALYSIS.jl/blob/main/Examples/tri_rods_dispersion.png 
--- src/plot_mpb.jl
+++ src/plot_mpb.jl
@@ -1,30 +1,31 @@
-function plot_mpb(filename::AbstractString, mode::AbstractString, nbands::Integer)
+function plot_mpb(filename::AbstractString, mode::AbstractString, plotorno::Bool=true; kwargs...)
     Bands = Vector{Vector{Float64}}()
     for line in readlines(filename)
-        if contains(line, mode*"freqs")
-            try
-                push!(Bands, parse.(Float64, string.(chop.(string.(split(line))))[end-nbands+1:end]))
-            catch 
-            end
-        end
+        contains(line, "k index") && continue
+        contains(line, mode*"freqs") || continue
+        line = replace(line, "," => "")
+        kpoint_freqs = split(line)[7:end] # Don't look at kpoint indices
+        kpoint_freqs = string.(kpoint_freqs) 
+        kpoint_freqs = parse.(Float64, kpoint_freqs) 
+        push!(Bands, kpoint_freqs)
     end
     numkpoints = length(Bands)
-    numbands = length(Bands[1])
+    numbands = length(first(Bands))
     reshapedBands = Array{Float64, 2}(undef, (numkpoints, numbands))
     for k in 1:numkpoints
        reshapedBands[k, :] = Bands[k]
     end
+    plotorno && (plot(reshapedBands; kwargs...); ylabel("Frequency"); xlabel("Wavevector"); xticks([]); title("Dispersion"))
     return reshapedBands
 end
 
 function plot_dispersion(filename::AbstractString)
     bands = readdlm(filename, ',')[:, 6:end]
-    bands_plot = Plots.plot(bands, legend=false, linewidth=5, )
+    bands_plot = plot(bands, legend=false, linewidth=5)
     title!(filename, titlefontsize=20)
-    display(bands_plot)
 end
 
-function plot_bothmodes(filename::AbstractString, nbands::Integer)
-    plot(plot_mpb(filename, "te", nbands), linewidth=5, color="red", legend=true, xticks=false, )
-    plot!(plot_mpb(filename, "tm", nbands), linewidth=5, color="blue", legend=false, xticks=false, ylabel="Frequency", label = "TM FREQS")
+function plot_bothmodes(filename::AbstractString; kwargs...)
+    plot_mpb(filename, "te"; color="red", label="TE Modes", kwargs...)
+    plot_mpb(filename, "tm"; color="green", label="TM Modes",  kwargs...)
 end
